3c0c0ad5a54a4820d85190db45eed02fcee9ac64
jeancarlos <jeancarlos.ingenieria@gmail.com>
2025-12-12 14:06:20 -0400

    feat: add LICENSE, change structured

---
+++ README.md
@@ -0,0 +1,11 @@
+# Personal WebSite
+
+WebSite personal fast portfolio and blog.
+
+Built with Astro, Tailwind and Typescript, an no frameworks.
+
+It was designed as an even more minimal theme than popular theme [Astro Nano](https://github.com/markhorn-dev/astro-nano/)
+
+## License
+
+- MIT
---
+++ astro.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://jeancarlos.dev",
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
